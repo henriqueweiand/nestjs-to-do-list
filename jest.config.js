1b6811e4ea8cf3e534b3d7d355a78043b3f005c7
Henrique Weiand <henriqueweiand@gmail.com>
2020-04-28 22:22:47 -0300

    test

--- jest.config.js
+++ jest.config.js
@@ -29,13 +29,5 @@ module.exports = {
     '!envs.ts',
     '!<rootDir>/main.ts',
   ],
-  coverageThreshold: {
-    global: {
-      branches: 95,
-      functions: 95,
-      lines: 95,
-      statements: 95,
-    },
-  },
   preset: 'ts-jest',
 };
